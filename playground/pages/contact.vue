c10932099f6824c4b952b5e349537c13b2911c06
Jowo <ignesia@gmail.com>
2025-03-13 16:21:38 +0100

    No redirect translation bug

--- playground/pages/contact.vue
+++ playground/pages/contact.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>contact</div>
+  <div><h1>{{ $t('contact.title') }}</h1></div>
 </template>
 
 <script setup>
 import { useNuxtApp } from '#imports'
 
 const { $defineI18nRoute } = useNuxtApp()
+
+definePageMeta({ name: 'contact' })
 
 $defineI18nRoute({
   localeRoutes: {
